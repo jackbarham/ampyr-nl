714184802924703272ce1ce0e605a987e822f99a
Jack Barham <jack@airboxmedia.com>
2023-10-24 14:33:02 +0100

    Menu desktop dropdown

--- components/TheHeader.vue
+++ components/TheHeader.vue
@@ -1,20 +1,35 @@
 <template>
   <header class="header absolute top-0 w-full z-50">
     <div class="flex justify-between w-full h-20 bg-gradient-to-b from-[#181F49] items-center px-12">
+      
       <NuxtLink to="/" class="w-32 h-8 shrink-0">
         <svgo-logo-colour />
       </NuxtLink>
-      <div class="nav">
-        <NuxtLink 
+
+      <ul class="nav flex">
+        <li 
           v-for="(page, index) in pages" 
           :key="index" 
-          :to="page.to" 
-          class="px-6 py-4 text-white tracking-wide"
-        >{{ page.name }}</NuxtLink>
-      </div>
+          class="group relative px-5 py-3 text-white tracking-wide hover:bg-opacity-10 hover:bg-white rounded"
+        >
+          <NuxtLink v-if="page.to" :to="page.to">{{ page.parent }}</NuxtLink>
+          <span v-if="!page.to" class="cursor-pointer">{{ page.parent }}</span>
+          <ul class="nav-sub absolute left-0 mt-2 w-[200px] bg-white text-black hidden group-hover:block rounded">
+            <li 
+              v-for="(link, subIndex) in page.links" 
+              :key="subIndex"
+              class=""
+            >
+              <NuxtLink v-if="link.to" :to="link.to" class="block px-4 py-2 hover:bg-gray-200 rounded">{{ link.page }}</NuxtLink>
+            </li>
+          </ul>
+        </li>
+      </ul>
+
       <div class="w-32 text-right shrink-0">
         <span class="text-white uppercase font-light tracking-widest">Regions</span>
       </div>
+
     </div>
   </header>
 </template>
@@ -22,32 +37,63 @@
 <script setup>
   const pages = [
     {
+      parent: 'Partner With Us',
+      links: [
+        {
+          page: 'Partner',
+          to: '/partner',
+        },
+        {
+          page: 'Partner',
+          to: '/partner',
+        },
+        {
+          page: 'Partner',
+          to: '/partner',
+        },
+      ]
+    },
+    {
+      parent: 'Energy Solutions',
+      links: [
+        {
+          page: 'Partner',
+          to: '/partner',
+        },
+        {
+          page: 'Partner',
+          to: '/partner',
+        },
+        {
+          page: 'Partner',
+          to: '/partner',
+        },
+      ]
+    },
+    {
+      parent: 'Portfolio',
       to: '/partner',
-      name: 'Partner With Us'
     },
     {
-      to: '/energy-solutions',
-      name: 'Energy Solutions'
+      parent: 'About Us',
+      links: [
+        {
+          page: 'Partner',
+          to: '/partner',
+        },
+        {
+          page: 'Partner',
+          to: '/partner',
+        },
+      ]
     },
     {
-      to: '/case-studies',
-      name: 'Case Studies'
-    },
-    {
-      to: '/about',
-      name: 'About Us'
-    },
-    {
-      to: '/insights',
-      name: 'Insights'
-    },
-    {
+      parent: 'Join Us',
       to: '/careers',
-      name: 'Join Us'
     },
     {
+      parent: 'Contact',
       to: '/contact',
-      name: 'Contact'
     },
   ]
 </script>
