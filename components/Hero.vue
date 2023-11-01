772446e5695bea1f4ba256cb8165ccc97f58a702
Jack Barham <jack@airboxmedia.com>
2023-11-01 12:27:25 +0000

    Homepage hero intro

--- components/Hero.vue
+++ components/Hero.vue
@@ -1,7 +1,16 @@
 <template>
   <div class="hero">
     <div class="c-bg-navy">
-      <img src="https://static.jackbarham.com/ampyr/hero.jpg" alt="Hero" class="object-cover h-[900px] w-full opacity-70">
+      <div class="absolute z-10 w-full">
+        <div class="flex items-center h-screen w-full max-w-6xl m-auto p-4 lg:p-8">
+          <div class="">
+            <h2 class="text-6xl font-normal mb-6">Hero Text</h2>
+            <p class="mb-6 text-lg max-w-sm">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+            <Button />
+          </div>
+        </div>
+      </div>
+      <img src="https://static.jackbarham.com/ampyr/hero.jpg" alt="Hero" class="object-cover w-full h-screen opacity-50">
     </div>
   </div>
 </template>
