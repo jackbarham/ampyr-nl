68c59162acf8f74987fcf1939c4aae84db25277b
Jack Barham <jack@airboxmedia.com>
2023-11-05 15:28:27 +0000

    CardsArticle

---
+++ components/CardsArticle.vue
@@ -0,0 +1,56 @@
+<template>
+  <div class="cards-article relative">
+    <div class="absolute h-8 lg:h-12 -top-8 lg:-top-12 left-0 w-full c-bg-navy z-50" style="clip-path: ellipse(50% 100% at 50% 100%);"></div>
+    <div class="c-bg-navy py-8 lg:py-16">
+      <div class="max-w-xl lg:max-w-7xl m-auto px-4">
+        <h1 class="text-white text-center text-3xl md:text-4xl lg:text-5xl mb-12 lg:mb-20 max-w-md lg:max-w-full m-auto">Latest from us...</h1>
+        <div class="lg:grid lg:grid-cols-3 lg:gap-6 mb-12 lg:mb-20">
+          <a 
+            v-for="(card, index) in cards" 
+            :key="index" 
+            class="block mb-6 lg:mb-0 bg-white hover:c-bg-navy-dark scale-1 lg:hover:scale-[103%] rounded-2xl overflow-hidden group transition-all"
+            :href="card.link"
+          >
+            <div class="relative">
+              <span class="bg-white c-navy text-xs px-3 py-1 rounded-full absolute z-10 right-4 top-4 capitalize">{{ card.category }}</span>
+              <img :src="card.image" alt="Description" class="w-full h-52 lg:h-60 object-cover">
+            </div>
+            <div class="relative p-6 h-52 lg:h-60">
+              <h2 class="c-navy group-hover:text-white text-xl tracking-wide">{{ card.heading }}</h2>
+              <div class="absolute bottom-6 left-6 w-10 h-10 border border-orange rounded-full p-0.5 bg-transparent group-hover:c-bg-orange">
+                <svgo-arrow-right />
+              </div>
+            </div>
+          </a>
+        </div>
+        <div class="flex justify-center">
+          <Button />
+        </div>
+        
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup>
+  const cards = [
+    {
+      heading: 'AMPYR Solar Europe And Asahi Europe & International Sign PPA For Dutch Drinks Production',
+      link: '#',
+      category: 'Press release',
+      image: 'https://static.jackbarham.com/ampyr/hero.jpg',
+    },
+    {
+      heading: 'Echt-Susteren & Venray — Construction Update',
+      link: '#',
+      category: 'Press release',
+      image: 'https://static.jackbarham.com/ampyr/hero.jpg',
+    },
+    {
+      heading: 'AMPYR Solar Europe secures €200M Project Finance Facility from Rabobank',
+      link: '#',
+      category: 'Press release',
+      image: 'https://static.jackbarham.com/ampyr/hero.jpg',
+    },
+  ]
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -4,4 +4,5 @@
   <ImageTextMiddle />
   <Statistics />
   <ImageTextLeft />
+  <CardsArticle />
 </template>
