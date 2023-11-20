9b5e56d7ac8c6908488c917cef832cee33f68684
Jack Barham <jack@airboxmedia.com>
2023-11-20 10:57:44 +0000

    Ground-based Solar Generation

--- pages/ground-based-solar-generation.vue
+++ pages/ground-based-solar-generation.vue
@@ -1,5 +1,14 @@
 <template>
   <HeroPage :content="hero" />
+  <ImageTextMiddle :content="imageTextMiddle" />
+  <Numbers />
+  <ImageHalf :content="imageHalf" />
+  <FeaturedSlider />
+  <Testimonial />
+  <ImageTextLeft :content="sustainability" />
+  <PeopleSlider />
+  <ImageTextLeft :content="powerDistribution" />
+  <CardsArticle />
 </template>
 
 <script setup>
@@ -18,6 +27,36 @@ const hero = {
     img3: 'https://static.jackbarham.com/ampyr/hero.jpg',
     svg: HeroSvg1,
   },
+}
+
+const imageTextMiddle = {
+  text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea.',
+  image: 'https://static.jackbarham.com/ampyr/hero.jpg',
+  alt: 'Image description',
+}
+
+const imageHalf = {
+  heading: 'Expanding on the solution',
+  text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+  image: 'https://static.jackbarham.com/ampyr/hero.jpg',
+}
+
+const sustainability = {
+  heading: 'Our Commitment To Sustainability',
+  text: 'We develop land for ground-mounted PV installations in a holistic way, taking into account the needs of each particular region to ensure sustainable power generation. For local communities and regions, we strive to help accelerate their journey to zero-carbon.',
+  buttonText: 'Find out more',
+  buttonLink: 'sustainability',
+  image: 'https://static.jackbarham.com/ampyr/hero.jpg',
+  alt: 'Description of image',
+}
+
+const powerDistribution = {
+  heading: 'Power Distribution',
+  text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+  buttonText: 'Find out more',
+  buttonLink: 'power-distribution',
+  image: 'https://static.jackbarham.com/ampyr/hero.jpg',
+  alt: 'Description of image',
 }
 
 onMounted(() => {
