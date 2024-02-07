fe89115e974918c413df2fc15c7dae72e7b54668
Jack Barham <jack@airboxmedia.com>
2024-02-07 16:41:35 +0000

    HeroFull CMS

---
+++ storyblok/HeroFull.vue
@@ -0,0 +1,37 @@
+<template>
+  <section v-editable="blok" class="hero-full">
+    <div class="bg-brand-navy">
+      <div class="absolute z-10 w-full">
+        <div class="flex items-center h-screen w-full max-w-6xl m-auto p-6">
+          <div class="text-white">
+            <h2 class="text-6xl mb-6">{{ blok.heading }}</h2>
+            <p class="mb-6 text-xl max-w-sm">{{ blok.text }}</p>
+            <Button theme="light" :link="blok.button[0].link" :text="blok.button[0].label" />
+            <div class="absolute right-0 top-0 h-full opacity-70 hidden lg:block">
+              <svgo-curve-hero filled class="" />
+            </div>
+          </div>
+        </div>
+      </div>
+      <img :src="blok.image" alt="#" class="object-cover w-full h-screen opacity-50">
+    </div>
+  </section>
+</template>
+
+<script setup>
+defineProps({
+  blok: {
+    type: Object,
+    required: true,
+  }
+})
+
+// const content = {
+//   heading: 'Land to Power',
+//   text: 'A leading European provider of sustainable solar energy solutions.',
+//   image: 'https://static.jackbarham.com/ampyr/hero.jpg',
+//   alt: 'Image description',
+//   buttonLink: '#',
+//   buttonText: 'Button not linked',
+// }
+</script>
