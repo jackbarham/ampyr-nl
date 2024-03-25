85b96c1e60c0f0b97b3111a164d42961ca2143a7
Jack Barham <jack@airboxmedia.com>
2024-03-25 12:38:04 +0000

    Image ratio fix

--- components/Hero/Variation1.vue
+++ components/Hero/Variation1.vue
@@ -2,20 +2,23 @@
   <div class="relative pb-[100%]">
     <NuxtImg 
       width="350"
+      height="350"
       :src="props.shapes.img1.filename" 
-      :alt="props.shapes.img1.title ? props.shapes.img1.title : 'alt title needed'" 
+      :alt="props.shapes.img1.alt" 
       class="absolute w-[60%] h-[60%] top-0 right-0 z-40 rounded-full" 
     />
     <NuxtImg 
       width="150"
+      height="150"
       :src="props.shapes.img2.filename" 
-      :alt="props.shapes.img2.title ? props.shapes.img2.title : 'alt title needed'" 
+      :alt="props.shapes.img1.alt" 
       class="absolute w-[24%] h-[24%] bottom-[10%] right-[10%] z-10 rounded-full" 
     />
     <NuxtImg 
       width="250"
+      height="250"
       :src="props.shapes.img3.filename" 
-      :alt="props.shapes.img3.title ? props.shapes.img3.title : 'alt title needed'" 
+      :alt="props.shapes.img1.alt" 
       class="absolute w-[42%] h-[42%] bottom-0 left-[5%] z-10 rounded-full" 
     />
     <div class="absolute w-[50%] h-[70%] bottom-[20%] left-[10%] z-20">
